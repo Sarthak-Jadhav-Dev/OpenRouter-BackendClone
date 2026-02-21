ada83a40c505b96835f90adb55a92b85b9317f07
Sarthak-Jadhav-Dev <sarthakjadhav4848@gmail.com>
2026-02-21 11:53:18 +0530

    ENV Var Fix

--- src/provider/deepseek.provider.js
+++ src/provider/deepseek.provider.js
@@ -1,7 +1,7 @@
 const OpenAI = require("openai");
 
 const openai = new OpenAI({
-    apiKey: process.env.GEMINI_API_KEY || "",
+    apiKey: process.env.DEEPSEEK_API_KEY || "",
     baseURL: "https://api.deepseek.com"
 });
 
